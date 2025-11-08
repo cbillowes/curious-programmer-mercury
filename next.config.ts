6eed530e722d6662f79d3253b74690d9c785bd94
Clarice Bouwer <cbillowes@gmail.com>
2025-11-08 13:13:57 +0400

    Install and configure flowbite

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from 'next';
 import { withContentCollections } from "@content-collections/next";
+import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
 // withContentCollections must be the outermost plugin
-export default withContentCollections(nextConfig);
+export default withFlowbiteReact(withContentCollections(nextConfig));
