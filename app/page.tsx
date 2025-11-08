57b31576e95caca40dcd0bb8f762b84761f525b2
Clarice Bouwer <cbillowes@gmail.com>
2025-11-08 18:28:05 +0400

    Fix text colors & change separator color

--- app/page.tsx
+++ app/page.tsx
@@ -183,7 +183,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <hr className="dark:border-pink-900 border-pink-400" />
+      <hr className="dark:border-blue-900 border-blue-400" />
       <section className="bg-gray-100 dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-7xl sm:py-16 lg:px-6">
           <div className="max-w-3xl mb-8 lg:mb-16">
@@ -211,7 +211,7 @@ export default function Home() {
                   ></path>
                 </svg>
               </div>
-              <h3 className="mb-2 text-xl font-bold text-black dark:text-white text-gray-900">
+              <h3 className="mb-2 text-xl font-bold text-black dark:text-white">
                 Soft Skills
               </h3>
               <ul className="my-6 lg:mb-0 space-y-4">
@@ -330,7 +330,7 @@ export default function Home() {
                   <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path>
                 </svg>
               </div>
-              <h3 className="mb-2 text-xl font-bold text-black dark:text-white text-gray-900">
+              <h3 className="mb-2 text-xl font-bold text-black dark:text-white">
                 Software Engineering
               </h3>
               <ul className="my-6 lg:mb-0 space-y-4">
@@ -437,7 +437,7 @@ export default function Home() {
                   <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path>
                 </svg>
               </div>
-              <h3 className="mb-2 text-xl font-bold text-black dark:text-white text-gray-900">
+              <h3 className="mb-2 text-xl font-bold text-black dark:text-white">
                 Engineering Toolkit
               </h3>
               <ul className="my-6 lg:mb-0 space-y-4">
@@ -550,7 +550,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <hr className="dark:border-pink-900 border-pink-400" />
+      <hr className="dark:border-blue-900 border-blue-400" />
       <aside
         aria-label="Related articles"
         className="py-8 bg-white dark:bg-gray-900 lg:py-16 antialiased"
