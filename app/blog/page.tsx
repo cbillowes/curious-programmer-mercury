82e42760dbc7492920b8b2eab93390ece5cb4d20
Clarice Bouwer <cbillowes@gmail.com>
2025-11-11 05:32:10 +0400

    Add page heading to full article listing page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,13 @@
 import { Page } from '@/components/page';
 import { Container } from '@/components/container';
+import { PageHeading } from '@/components/page-heading';
 
 export default async function ArticleListingPage() {
   return (
     <Page>
-      <Container>Articles Page</Container>
+      <Container>
+        <PageHeading>Articles</PageHeading>
+      </Container>
     </Page>
   );
 }
