c47d0f75da6561e6ddca0144d348d80d0e21a1c1
Clarice Bouwer <cbillowes@gmail.com>
2025-11-12 05:29:19 +0400

    Remove duplicate page heading on article page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -60,7 +60,6 @@ export default async function ArticlePage({ params }: Props) {
       return (
         <Page>
           <Container>
-            <PageHeading>{data.title}</PageHeading>
             <Content {...data} />
           </Container>
         </Page>
