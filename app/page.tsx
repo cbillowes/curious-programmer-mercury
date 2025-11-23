109b94709ee3e4eb2c91ccedbe895f238c6a8c3c
Clarice Bouwer <cbillowes@gmail.com>
2025-11-23 21:13:20 +0400

    Reduce page width on home page for about content

--- app/page.tsx
+++ app/page.tsx
@@ -81,7 +81,7 @@ export default function Home() {
   return (
     <Page>
       <section className="bg-gray-50 dark:bg-gray-900 py-5">
-        <div className="px-4 sm:px-32 grid max-w-7xl xl:px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="px-4 sm:px-32 grid max-w-5xl xl:px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
             <div className="lg:hidden flex items-center justify-start">
               <Image
