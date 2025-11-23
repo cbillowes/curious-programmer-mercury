fc179095648d930239c2534bfb68c7c0f41c4079
Clarice Bouwer <cbillowes@gmail.com>
2025-11-23 05:03:42 +0400

    Add search indexer

---
+++ scripts/search.js
@@ -0,0 +1,15 @@
+const search = require('algoliasearch');
+
+const client = search.algoliasearch('V33FO4456K', '69bf87d6f01bae7bc3472545cc4b2a81');
+
+fetch('http://localhost:3000/api/search')
+  .then((res) => res.json())
+  .then((content) => {
+    return client.saveObjects({ indexName: 'Pages', objects: content });
+  })
+  .then(() => {
+    console.log('Successfully indexed objects!');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
