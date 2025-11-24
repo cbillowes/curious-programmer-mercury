937678767a837bd30e28078735c71ac561366a00
Clarice Bouwer <cbillowes@gmail.com>
2025-11-24 07:44:54 +0400

    Use API_KEY from env for algoliasearch indexing

--- scripts/search.js
+++ scripts/search.js
@@ -1,6 +1,6 @@
 const search = require('algoliasearch');
 
-const client = search.algoliasearch('V33FO4456K', '69bf87d6f01bae7bc3472545cc4b2a81');
+const client = search.algoliasearch('V33FO4456K', process.env.ALGOLIA_API_KEY);
 
 fetch('https://curiousprogrammer.dev/api/search')
   .then((res) => res.json())
