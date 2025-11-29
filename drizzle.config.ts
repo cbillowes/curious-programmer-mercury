9cb0b027461d3291dbbfda04ebc16847d16cd0b0
Clarice Bouwer <cbillowes@gmail.com>
2025-11-29 15:49:22 +0400

    Use NETLIFY_DATABASE_URL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   schema: './db/schema.ts',
   out: './drizzle',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.NETLIFY_DATABASE_URL!,
   },
 });
