7795873771e097ec4edf72f1b69d37444a515b81
Clarice Bouwer <cbillowes@gmail.com>
2025-11-29 16:25:27 +0400

    Use grid for sections on Likes and Bookmarks

--- app/my/bookmarks/page.tsx
+++ app/my/bookmarks/page.tsx
@@ -74,7 +74,7 @@ export default async function MyBookmarksPage() {
           showType={true}
           empty={<EmptyContents />}
         />
-        <div className="flex gap-4 my-4">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 my-4">
           {sections.map((section) => (
             <Link
               key={section.href}
