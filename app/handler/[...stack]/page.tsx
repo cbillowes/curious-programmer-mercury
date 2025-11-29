ae7ef0b805287f9c150de892f7bd37cb2032b1fc
Clarice Bouwer <cbillowes@gmail.com>
2025-11-29 15:55:22 +0400

    Add meta data to handler page

--- app/handler/[...stack]/page.tsx
+++ app/handler/[...stack]/page.tsx
@@ -1,5 +1,16 @@
 import { Page } from '@/components/page';
+import { getPageMetadata } from '@/lib/utils';
 import { StackHandler } from '@stackframe/stack';
+
+export async function generateMetadata() {
+  return getPageMetadata({
+    title: 'Account | Curious Programmer',
+    description: 'Manage your account and preferences on Curious Programmer.',
+    slug: 'handler',
+    image: '/home.png',
+    type: 'website',
+  });
+}
 
 export default function Handler() {
   return (
