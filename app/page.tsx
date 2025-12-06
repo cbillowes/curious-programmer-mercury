59d8883022b8b3e548e702b48649da95a9b016c9
Clarice Bouwer <cbillowes@gmail.com>
2025-12-07 03:06:33 +0400

    Fix about me button on home page

--- app/page.tsx
+++ app/page.tsx
@@ -135,7 +135,7 @@ export default async function Home() {
             </p>
             <Link
               href="/resume"
-              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-black dark:text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
             >
               About me
               <svg
