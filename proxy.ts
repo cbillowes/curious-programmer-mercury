9400566b166f55775187759b8fea5effbe08fea8
Clarice Bouwer <cbillowes@gmail.com>
2025-12-07 03:33:18 +0400

    Redirect /about to /resume

--- proxy.ts
+++ proxy.ts
@@ -3,6 +3,11 @@ import type { NextRequest } from 'next/server';
 
 // https://nextjs.org/docs/messages/middleware-to-proxy
 export function proxy(request: NextRequest) {
+  if (request.nextUrl.pathname === '/about') {
+    request.nextUrl.pathname = '/resume';
+    return NextResponse.redirect(request.nextUrl);
+  }
+
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set('x-url', request.url);
 
