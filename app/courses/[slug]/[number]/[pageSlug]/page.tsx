3658f7fda8da7eae3d70562aea3cc42ed0cadf95
Clarice Bouwer <cbillowes@gmail.com>
2025-12-06 13:57:42 +0400

    Hide course page behind auth keeping meta data open for sharing

--- app/courses/[slug]/[number]/[pageSlug]/page.tsx
+++ app/courses/[slug]/[number]/[pageSlug]/page.tsx
@@ -2,11 +2,13 @@ import { Page } from '@/components/page';
 import { Container } from '@/components/container';
 import { CoursePageContent } from '@/components/content';
 import { getCoursePageBySlug } from '@/lib/courses';
-import { notFound } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
 import { Hero } from '@/components/hero';
 import { getPageMetadata } from '@/lib/utils';
 import { getBookmarks } from '@/db/bookmark';
 import { getLikes } from '@/db/likes';
+import { headers } from 'next/headers';
+import { stackServerApp } from '@/stack/server';
 
 function getPageSlug(slug: string, number: string, pageSlug: string) {
   return `/courses/${slug}/${number.padStart(2, '0')}/${pageSlug}`;
@@ -36,6 +38,17 @@ export async function generateMetadata({ params }: Props) {
 }
 
 export default async function CoursePagePage({ params }: Props) {
+  // Hide page behind authentication
+  const headersList = await headers();
+  const urlString = headersList.get('x-url');
+  const user = await stackServerApp.getUser();
+  if (!user) {
+    const currentPath = urlString ? new URL(urlString).pathname : '/';
+    const encodedPath = encodeURIComponent(currentPath);
+    const returnUrl = `${stackServerApp.urls.signIn}?after_auth_return_to=${encodedPath}`;
+    return redirect(returnUrl);
+  }
+
   const { slug, number, pageSlug } = await params;
   const data = getCoursePageBySlug(getPageSlug(slug, number, pageSlug));
   if (!data) notFound();
