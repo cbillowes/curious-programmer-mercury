1d5e72167bccc845280500c12b2ecaf404adb8df
Clarice Bouwer <cbillowes@gmail.com>
2025-12-07 02:31:06 +0400

    Order resume items in date desc

--- lib/resume.ts
+++ lib/resume.ts
@@ -1,7 +1,12 @@
 import { allResumes } from 'content-collections';
 
 export function getResume() {
-  return allResumes;
+  return allResumes.sort((a, b) => {
+    if (a.date && b.date) {
+      return new Date(b.date).getTime() - new Date(a.date).getTime();
+    }
+    return 0;
+  });
 }
 
 export function getResumeBySlug(slug: string) {
