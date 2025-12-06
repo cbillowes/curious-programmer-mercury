1ae1c25df30e68c87aac3183ecdd1dbabdc2ae6d
Clarice Bouwer <cbillowes@gmail.com>
2025-12-07 03:30:34 +0400

    Add breathing room between theme toggle & avatar in header

--- components/header.tsx
+++ components/header.tsx
@@ -186,7 +186,7 @@ export function Header() {
               <div className="print:hidden flex items-center gap-2">
                 <Search />
                 <button
-                  className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-lg p-2.5"
+                  className="mr-2 text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-lg p-2.5"
                   onClick={toggleMode}
                 >
                   <span className="sr-only">Toggle theme</span>
