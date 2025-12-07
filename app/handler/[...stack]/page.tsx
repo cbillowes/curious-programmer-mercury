25199f1d1af1c577ef360099704e4b4feb310a1d
Clarice Bouwer <cbillowes@gmail.com>
2025-12-07 12:11:43 +0400

    Reduce page width of Stack Handler page

--- app/handler/[...stack]/page.tsx
+++ app/handler/[...stack]/page.tsx
@@ -5,7 +5,8 @@ import { StackHandler } from '@stackframe/stack';
 export async function generateMetadata() {
   return getPageMetadata({
     title: 'Account | Curious Programmer',
-    description: 'Your gateway to exclusive content and personalized settings on Curious Programmer.',
+    description:
+      'Your gateway to exclusive content and personalized settings on Curious Programmer.',
     slug: 'handler',
     image: '/home.png',
     type: 'website',
@@ -15,7 +16,9 @@ export async function generateMetadata() {
 export default function Handler() {
   return (
     <Page>
-      <StackHandler fullPage />
+      <div className="max-w-5xl mx-auto px-5 py-8">
+        <StackHandler fullPage />
+      </div>
     </Page>
   );
 }
