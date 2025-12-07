56f09a2e26c3a2fb5a858a2ab1acc6f0d693430b
Clarice Bouwer <cbillowes@gmail.com>
2025-12-07 12:09:47 +0400

    Update SEO for Stack Handler page

--- app/handler/[...stack]/page.tsx
+++ app/handler/[...stack]/page.tsx
@@ -5,7 +5,7 @@ import { StackHandler } from '@stackframe/stack';
 export async function generateMetadata() {
   return getPageMetadata({
     title: 'Account | Curious Programmer',
-    description: 'Manage your account and preferences on Curious Programmer.',
+    description: 'Your gateway to exclusive content and personalized settings on Curious Programmer.',
     slug: 'handler',
     image: '/home.png',
     type: 'website',
