10bf3da859a5c61983b052068d724d43c58c2a60
Clarice Bouwer <cbillowes@gmail.com>
2025-12-08 09:32:01 +0400

    Fix aspect ratio of Backdrop

--- components/backdrop.tsx
+++ components/backdrop.tsx
@@ -3,22 +3,22 @@ import Image from 'next/image';
 export function Backdrop() {
   return (
     <div className="absolute z-20 top-0 inset-x-0 flex justify-center overflow-hidden pointer-events-none print:hidden">
-      <div className="w-432 flex-none flex justify-end blur opacity-70">
+      <div className="w-full flex-none flex justify-end blur opacity-70">
         <Image
           src="/backdrop_1.png"
           alt="Gradient background"
-          className="w-287 flex-none max-w-none dark:hidden"
+          className="w-full flex-none max-w-none dark:hidden"
           priority={true}
-          height={287}
-          width={432}
+          height={668}
+          width={2296}
         />
         <Image
           src="/backdrop_2.png"
           alt="Gradient background"
-          className="w-360 flex-none max-w-none hidden dark:block"
+          className="w-full flex-none max-w-none hidden dark:block"
           priority={true}
-          height={287}
-          width={432}
+          height={668}
+          width={2296}
         />
       </div>
     </div>
