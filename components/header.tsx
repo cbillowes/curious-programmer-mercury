8f7b3d19329b9f12c3c37b706e06e7dd55ebc631
Clarice Bouwer <cbillowes@gmail.com>
2025-12-08 08:31:34 +0400

    Change aria for ToggleSidebar
    Assistive technologies, such as screen readers, can't interpret ARIA attributes with invalid values.

--- components/header.tsx
+++ components/header.tsx
@@ -110,8 +110,7 @@ function ToggleSidebar({
 }) {
   return (
     <button
-      aria-expanded="true"
-      aria-controls="sidebar"
+      aria-expanded={isOpen}
       className="print:hidden p-2 text-gray-600 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
       onClick={toggle}
     >
