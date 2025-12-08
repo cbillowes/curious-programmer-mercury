e18b2f284590d3d532d9fc7137117da20e32e0a4
Clarice Bouwer <cbillowes@gmail.com>
2025-12-08 08:05:46 +0400

    Fix Ribbon: Background and foreground colors to have sufficient contrast ratio
    Low-contrast text is difficult or impossible for many users to read.

--- components/ribbon.tsx
+++ components/ribbon.tsx
@@ -6,7 +6,7 @@ export function Ribbon({ children }: { children?: ReactNode }) {
     <div className="ribbon absolute right-0 z-40">
       <div className="backdrop absolute overflow-hidden inline-block h-36 w-36 top-0 -right-1">
         <div className="bg-pink-600 w-52 h-10 absolute top-10 -right-12 overflow-hidden transform rotate-45 py-1 text-center font-bold">
-          <div className="border-dashed border-b border-l border-r border-t border-pink-200 mb-1 pb-1 text-pink-200">
+          <div className="border-dashed border-b border-l border-r border-t border-white mb-1 pb-1 text-white">
             {children}
           </div>
         </div>
